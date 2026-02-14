ba5b75a9279b6a2248eb2ccfd4a4b92440ddceb2
HeshamMera <hesham.mera@gmail.com>
2026-02-15 01:33:57 +0200

    Fix Laravel bootstrap for Vercel serverless environment

--- api/index.php
+++ api/index.php
@@ -1,3 +1,11 @@
 <?php
 
+// Set the application base path
+$_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../public/index.php';
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+
+// Change working directory to public folder
+chdir(__DIR__ . '/../public');
+
+// Bootstrap Laravel
 require __DIR__ . '/../public/index.php';
